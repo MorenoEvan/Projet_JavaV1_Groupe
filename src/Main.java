2777e28ec480063fe31c46e39f4662a36490ebcc
MorenoEvan <EvanMorenoDeux@gmail.com>
2023-03-23 10:21:05 +0100

    Ok

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Vues.FrmMenu;
+
+public class Main {
+    public static void main(String[] args){
+        FrmMenu frm = new FrmMenu();
+        frm.setVisible(true);
+    }
+}
--- src/Vues/FrmMenu.java
+++ src/Vues/FrmMenu.java
@@ -1,4 +1,20 @@
 package Vues;
 
-public class FrmMenu {
+import javax.swing.*;
+
+public class FrmMenu extends JFrame {
+    private JPanel pnlRoot;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JLabel lblCo;
+    private JLabel lblTitre;
+
+    public FrmMenu()
+    {
+        this.setTitle("V1");
+        this.setContentPane(pnlRoot);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+    }
 }
